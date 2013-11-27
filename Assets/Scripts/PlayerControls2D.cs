b0b0384f67a09cc1a93808f5657658255c487109
John Pennington <atmospherium@gmail.com>
2013-11-27 01:00:23 -0500

    Animation layers. Directional animation.

--- Assets/Scripts/PlayerControls2D.cs
+++ Assets/Scripts/PlayerControls2D.cs
@@ -4,10 +4,12 @@ using System.Collections;
 public class PlayerControls2D : MonoBehaviour {
 	private Animator animator;
 	float speed = 6f;
-
+	float xScale;
+	float scaleMod = 1;
 
 	void Start(){
 		animator = GetComponent<Animator>();
+		xScale = Mathf.Abs(transform.localScale.x);
 	}
 
 	// Update is called once per frame
@@ -16,6 +18,14 @@ public class PlayerControls2D : MonoBehaviour {
 		bool j = Input.GetButtonDown("Jump");
 
 		float currentY = rigidbody2D.velocity.y;
+
+		if(h>0){
+			scaleMod = Mathf.Abs(scaleMod);
+		}else if(h<0){
+			scaleMod = -Mathf.Abs(scaleMod);
+		}
+
+		transform.localScale = new Vector2(xScale*scaleMod,transform.localScale.y);
 
 		if(j){
 			currentY = 5;
